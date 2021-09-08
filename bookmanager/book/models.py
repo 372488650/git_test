9198d81c724193ada57fa6331b5f0b255b731b62
beatsl <372488650@qq.com>
2021-09-08 01:47:26 -0700

    ///添加类

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    # Id
+    name = models.CharField(max_length=10)
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外健约束：人物属于那本书
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lnv1-#-2w=h)sk)y^l7!wn*j49!jc0h0-i$fxscvb_&77r)8*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book',
 ]
 
 MIDDLEWARE = [
